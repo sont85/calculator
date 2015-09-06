1d4de450b60433b2d2980ab08c6a9c21726d64cd
Son Truong <sont85@gmail.com>
2015-09-05 18:22:24 -0700

    square root, and plus minus functionality

--- calculator.js
+++ calculator.js
@@ -26,19 +26,52 @@ var calc = (function() {
       var secondNumbers = Number(currentNumbers);
       var result = calc[operator](numbers, secondNumbers);
       numbers = result;
+      return result;
+    },
+    equal: function(){
+      if (operator) {
+      var result = calc.calculate();
       calc.display(result);
+      calc.reset();
+      }
+    },
+    clear: function() {
+      calc.reset();
+      calc.numbers = null;
+      calc.display('0');
+    },
+    plusMinus: function() {
+      currentNumbers = Number(currentNumbers) * -1;
+      calc.display(currentNumbers || '0');
+    },
+    squareRoot: function() {
+      currentNumbers = Math.sqrt(Number(currentNumbers));
+      calc.display(currentNumbers || '0');
+    },
+    delete: function() {
+      console.log(currentNumbers, typeof currentNumbers)
+      currentNumbers.slice(currentNumbers.length - 1, 1)
+      calc.display();
+    },
+    storeNumbers: function(){
+      if (currentNumbers) {
+        numbers = Number(currentNumbers);
+      }
+      console.log(numbers);
+      currentNumbers = '';
     },
     storeOperator: function(mathOperator) {
-      console.log(mathOperator);
-      operator = mathOperator;
-    },
-    storeNumbers : function(){
-      numbers = Number(currentNumbers);
-      currentNumbers = '';
+      if (numbers) {
+        operator = mathOperator;
+      }
     },
     display: function(result) {
       var displayNumber = result || currentNumbers;
       $('#display').text(displayNumber);
+    },
+    reset: function() {
+      currentNumbers = '';
+      operator = null;
     }
   };
 })();
@@ -55,7 +88,9 @@ $(function() {
     calc.digit(number);
     calc.display();
   });
-  $('#equalButton').on('click', function() {
-    calc.calculate();
+
+  $('.calc-op').on('click', function(){
+    var op = $(this).data('op');
+    calc[op]();
   });
 });
